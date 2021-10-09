f10b428a85ddf63f76a83ae9966f7560d7337c54
Eph <ephearem@gmail.com>
2021-10-09 18:23:18 +0300

    Implement the ability to specify the position and size of textures (in pixels) The 'vertices' array stores the local coordinates of the vertices of two triangles (in the range [0..1]) and the corresponding texture coordinates. The coordinates of each vertex are passed to the 'in_pos' vector of the vertex shader (the order is determined by the contents of GL_ELEMENT_ARRAY_BUFFER, i.e. the 'indices' array). Also, uniforms vec2 'uf_model_pos' and vec2 'uf_model_size' are passed to the vertex shader. These variables set the position of the vertices (in pixels) and the size of the primitives formed from these vertices (in pixels), respectively. Position and size are set as follows: A 4x4 'model' matrix is created in the vertex shader. Scaling along the x and y axes is implemented by changing the first two elements of the main diagonal of this matrix (i.e. 'model[0][0]', 'model[1][1]'). Shifting along the x and y axes is implemented by changing the elements with indices [3][0], [3][1], respectively. Thus, scaling along the x and y axes in the vertex shader is implemented in the lines: model[0][0] = uf_model_size.x; model[1][1] = uf_model_size.y; , and the position change is implemented in the lines: model[3][0] = uf_model_pos.x; model[3][1] = uf_model_pos.y; To avoid distortions in further transformations, the remaining values of the main diagonal of the 'model' matrix (i.e., 'model[2][2]', 'model[3][3]') should be filled with ones, and everything else (except elements used for scaling and shifting) must be filled with zeros. The result of multiplying this matrix by the vector 'in_pos' is a coordinate vector in the world space coordinate system.
    In addition, the projection matrix (uniform variable 'uf_projection') is passed to the vertex shader. The projection matrix is created based on the window size by calling the 'glm_ortho' function and passed to the shader before the start of the render loop. Multiplication involving the projection matrix converts the coordinates given in pixels to normalized ones, that is, it "projects" the scene onto the working area of the window (i.e. the area specified by the 'glViewport' function).

--- CEphProject/src/main.c
+++ CEphProject/src/main.c
@@ -12,6 +12,9 @@
 #include <glad\glad.h>  // TODO: Remove.
 #include <GLFW/glfw3.h> // TODO: Remove.
 
+#pragma warning (disable: 4996)
+#include <cglm/cglm.h>
+
 #include "core/window.h"
 #include "core/loop.h"
 #include "core/graphics/shaders.h"
@@ -56,17 +59,26 @@ int main(int argc, char* argv[], char* envp[])
     /* Set up vertices and indices data */
     float vertices[] =
     {
-         0.5f,  0.5f, 1.0f, 1.0f,       /* Top right                          */
-         0.5f, -0.5f, 1.0f, 0.0f,       /* Bottom right                       */
-        -0.5f, -0.5f, 0.0f, 0.0f,       /* Bottom left                        */
-        -0.5f,  0.5f, 0.0f, 1.0f        /* Top left                           */
+        1.0f, 0.0f,                     /* Top right                          */
+        1.0f, 1.0f,                     /* Bottom right                       */
+        0.0f, 1.0f,                     /* Bottom left                        */
+        0.0f, 0.0f                      /* Top left                           */
     };
     unsigned int indices[] =
     {
         0, 1, 3,                        /* The 1-st triangle                  */
         1, 2, 3                         /* The 2-nd triangle                  */
     };
+    float txd_vertices[] =
+    {
+        1.0f, 1.0f,                     /* Top right                          */
+        1.0f, 0.0f,                     /* Bottom right                       */
+        0.0f, 0.0f,                     /* Bottom left                        */
+        0.0f, 1.0f                      /* Top left                           */
+    };
+
     unsigned int vertex_buffer = 0;
+    unsigned int txd_vertex_buffer = 0;
     unsigned int indices_buffer = 0;
     unsigned int vertex_array = 0;
 
@@ -74,6 +86,9 @@ int main(int argc, char* argv[], char* envp[])
 
     glGenBuffers(1, &vertex_buffer);    /* Generate a buffer object to store  */
                                         /* the positions of the vertices      */
+
+    glGenBuffers(1, &txd_vertex_buffer);/* Generate a buffer object to store  */
+                                        /* the texture coordinates            */
 
     glGenBuffers(1, &indices_buffer);   /* Generate a buffer object to store  */
                                         /* the vertex indices                 */
@@ -93,6 +108,18 @@ int main(int argc, char* argv[], char* envp[])
                                         /* GL_ARRAY_BUFFER (i.e. into         */
                                         /* 'vertex_buffer')                   */
 
+    glBindBuffer(GL_ARRAY_BUFFER, txd_vertex_buffer);
+                                        /* Bind 'vertex_buffer' to            */
+                                        /* GL_ARRAY_BUFFER. All following     */
+                                        /* calls to GL_ARRAY_BUFFER will      */
+                                        /* refer to this 'vertex_buffer'      */
+                                        /* object.                            */
+    glBufferData(GL_ARRAY_BUFFER, sizeof(txd_vertices), txd_vertices,
+        GL_STATIC_DRAW);
+                                        /* Put data from 'txd_vertices' into  */
+                                        /* GL_ARRAY_BUFFER (i.e. into         */
+                                        /* 'txd_vertex_buffer'                */
+
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indices_buffer);
                                         /* Bind 'vertex_buffer' to            */
                                         /* GL_ELEMENT_ARRAY_BUFFER. All       */
@@ -105,21 +132,22 @@ int main(int argc, char* argv[], char* envp[])
                                         /* GL_ELEMENT_ARRAY_BUFFER (i.e. into */
                                         /* 'indices_buffer')                  */
 
-    glBindVertexBuffer(0, vertex_buffer, 0, sizeof(GLfloat) * 4);
+    glBindVertexBuffer(0, vertex_buffer, 0, sizeof(GLfloat) * 2);
                                         /* Bind 'vertex_buffer' to            */
                                         /* 'vertex_array' at index 0.         */
 
-    glBindVertexBuffer(1, vertex_buffer, sizeof(GLfloat) * 2,
-        sizeof(GLfloat) * 4);           /* Bind 'vertex_buffer' to            */
+    glBindVertexBuffer(1, txd_vertex_buffer, 0, sizeof(GLfloat) * 2);
+                                        /* Bind 'txd_vertex_buffer' to        */
                                         /* 'vertex_array' at index 0.         */
 
     glEnableVertexAttribArray(0);
     glEnableVertexAttribArray(1);
 
-    glBindBuffer(GL_ARRAY_BUFFER, 0);   /* 'vertex_buffer' can be safely      */
-                                        /* unbind since it is bound to        */
+    glBindBuffer(GL_ARRAY_BUFFER, 0);   /* 'vertex_buffer' and                */
+                                        /* 'txd_vertex_buffer'can be unbound  */
+                                        /* since they are bound to            */
                                         /* 'vertex_array' as the vertex       */
-                                        /* attribute at index 0.              */
+                                        /* attributes at indices 0 and 1      */
 
     //glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
                                         /* 'indices_buffer' cannot be         */
@@ -222,8 +250,16 @@ int main(int argc, char* argv[], char* envp[])
     free_image(img_1);
     free_image(img_2);
 
+    mat4 projection = GLM_MAT4_IDENTITY_INIT;
+    glm_ortho(0.0f, (float)get_window_width(), (float)get_window_height(),
+        0.0f, -0.1f, 0.1f, projection);
+    set_shader_uf_mat4(shader_program, "uf_projection", projection);
     set_shader_uf_int(shader_program, "uf_txd_array_z_offset", 0);
     set_shader_uf_int(shader_program, "uf_txd_unit", 0);
+    vec2 pos = { 10.0f, 10.0f };
+    set_shader_uf_vec2(shader_program, "uf_model_pos", pos);
+    vec2 size = { 512.0f, 512.0f };
+    set_shader_uf_vec2(shader_program, "uf_model_size", size);
 
 
     start_loop(loop_iteration_callback);
@@ -231,8 +267,9 @@ int main(int argc, char* argv[], char* envp[])
     /* De-allocate all resources */
     glDeleteVertexArrays(1, &vertex_array);
     glDeleteBuffers(1, &vertex_buffer);
+    glDeleteBuffers(1, &txd_vertex_buffer);
     glDeleteBuffers(1, &indices_buffer);
-    glDeleteTextures(1, &texture_2d_array);
+    glDeleteTextures(1, &texture_2d_array); // TODO: Implement.
 
     return 0;
 }
