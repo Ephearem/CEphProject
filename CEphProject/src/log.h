d5b1ebc6e75618f120ee1bcb13f89456d8031037
Eph <ephearem@gmail.com>
2021-10-16 20:35:33 +0300

    Implement 'GL_CALL' macro to catch errors in GL-function calls

--- CEphProject/src/log.h
+++ CEphProject/src/log.h
@@ -24,4 +24,15 @@
 #define LOG_WARNING(...) printf("WARNING: " __VA_ARGS__); printf("\n");
 #define LOG_ERROR(...) printf("ERROR: " __VA_ARGS__); printf("\n");
 
+#define GL_CALL(expression) \
+do \
+{ \
+    expression; \
+    GLenum result = glGetError(); \
+    if(result != GL_NO_ERROR) \
+    { \
+        LOG_ERROR("Expression: %s\n\tError: %d\n\tFile: %s\n\tLine: %d", #expression,  result, __FILE__, __LINE__); \
+    } \
+}while (0)
+
 #endif /* LOG_H */
